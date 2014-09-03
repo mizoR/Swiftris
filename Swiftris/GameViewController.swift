d6333c61807906724775d598540d68341d6f9933
mizokami <suzunatsu@yahoo.com>
2014-09-04 05:17:41 +0900

    8. Let Them Fall

--- Swiftris/GameViewController.swift
+++ Swiftris/GameViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
+    var scene: GameScene!
+    var swiftris: Swiftris!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -9,14 +11,32 @@ class GameViewController: UIViewController {
         let skView = view as SKView
         skView.multipleTouchEnabled = false
         
-        var scene: GameScene!
         scene = GameScene(size: skView.bounds.size)
         scene.scaleMode = .AspectFill
         
+        scene.tick = didTick
+        swiftris = Swiftris()
+        swiftris.beginGame()
+        
         skView.presentScene(scene)
+        
+        scene.addPreviewShapeScreen(swiftris.nextShape!) {
+            self.swiftris.nextShape?.moveTo(StartingColumn, row: StartingRow)
+            self.scene.movePreviewShape(self.swiftris.nextShape!) {
+                let nextShapes = self.swiftris.newShape()
+                self.scene.startTicking()
+                self.scene.addPreviewShapeScreen(nextShapes.nextShape!) {}
+                
+            }
+        }
     }
 
     override func prefersStatusBarHidden() -> Bool {
         return true
+    }
+    
+    func didTick() {
+        swiftris.fallingShape?.lowerShapeByOneRow()
+        scene.redrawShape(swiftris.fallingShape!, completion: {})
     }
 }
